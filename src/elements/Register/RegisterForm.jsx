f594c534615cb94d2a934b4d90e2f7a54a996195
heypuni <gabbyevalinf@gmail.com>
2023-12-10 23:13:12 +0700

    loginregister pengacara

--- src/elements/Register/RegisterForm.jsx
+++ src/elements/Register/RegisterForm.jsx
@@ -1,47 +1,65 @@
-// import useNavigate from 'react-router-dom';
-// import { useState } from 'react';
-// import axios from 'axios';
+import useNavigate from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import React from 'react';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 const RegisterForm = () => {
 
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const navigate = useNavigate();
+  const [token, setToken] = useState(null)
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [registerName, setRegisterName] = useState('');
+  const [registerKtp, setRegisterKtp] = useState(''); // eslint-disable-line no-unused-vars
+  const navigate = useNavigate();
 
-  // const handleRegisterLinkClick = (e) => {
-  //   e.preventDefault();
-  //   const data = {email, password};
-  //   console.log(data);
-  //   navigate('/');
-  // }
+  const handleLoginLinkClick = (e) => {
+    e.preventDefault();
+    const data = {registerEmail, registerPassword, registerName, registerKtp};
+    console.log(data);
+    navigate('/login');
+  }
 
-  // // eslint-disable-next-line no-undef
-  // const URL = process.env.REACT_APP_BE_ENDPOINT
-  // console.log('URL', URL)
+  // eslint-disable-next-line no-undef
+  const URL = process.env.REACT_APP_BE_ENDPOINT
+  console.log('URL', URL)
 
-  // const onLoginSubmit = () => {
-  //   console.log(`email: ${email}, password: ${password}`)
-  //     axios.post(`${URL}/v1/auth/login`, {
-  //       email,
-  //       password
-  //     })
-  //       .then(function (response) {
-  //         console.log('login success', response);
-  //           if (response && response.data && response.data.tokens && response.data.tokens.access) {
-  //             localStorage.setItem('token', JSON.stringify(response.data.tokens.access.token))
-  //             localStorage.setItem('user', JSON.stringify(response.data.user))
-  //             navigate('/homepage')
-  //           }
-  //       })
-  //         .catch(function (error) {
-  //           console.error('Login error', error.message);
-  //            window.alert('error', 'Login Failed', `<b>[CODE] ${error.code}</b><br>Please check your username and password`);
-  //           }
-  //       );
-  // }
+  const onRegisterSubmit = () => {
+    console.log(`name: ${registerName}, email: ${registerEmail}, password: ${registerPassword}, ktp: ${registerKtp}`)
+    axios.post(`${URL}/v1/auth/register`, {
+      name: registerName,
+      email: registerEmail,
+      ktp: registerKtp,
+      password: registerPassword
+    })
+      .then(function (response) {
+        console.log('register success', response);
+        if (response && response.data && response.data.tokens && response.data.tokens.access) {
+          localStorage.setItem('token', JSON.stringify(response.data.tokens.access.token))
+          localStorage.setItem('user', JSON.stringify(response.data.user))
+          navigate ('/login');
+        }
+      })
+      .catch(function (error) {
+        if (error.response && error.response.status === 400 && error.response.data.message) {
+            window.alert('error', 'Registration Failed', `<b>[CODE] ${error.response.data.message}</b><br>Please check your username and password`);
+        } else {
+            window.alert('error', 'Registration Failed', `<b>[CODE] </b><br>An error occured during registration. Please try again later`);
+        }
+        console.error('Registration error', error.message);
+     }
+     );
+   }
+
+  console.log('data', registerName, registerEmail, registerPassword);
+  
+  useEffect(() => {
+    const tokenFromLS = localStorage.getItem('token')
+    setToken(tokenFromLS)
+  }, [])
+
+  console.log('token', token)
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
@@ -54,27 +72,27 @@ const RegisterForm = () => {
             <form className="space-y-4 md:space-y-6" action="">
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Full Name</label>
-                <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your full name here' required=""/>
+                <input  onChange={(e) => setRegisterName(e.target.value)} type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your full name here' required=""/>
               </div>
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
-                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your email here' required=""/>
+                <input onChange={(e) => setRegisterEmail(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your email here' required=""/>
               </div>
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your NIK (KTP)</label>
-                <input type="ktp" name="ktp" id="ktp" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your NIK here' required=""/>
+                <input onChange={(e) => setRegisterKtp(e.target.value)} type="ktp" name="ktp" id="ktp" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your NIK here' required=""/>
               </div>
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" placeholder='Write your password here'/>
+                <input onChange={(e) => setRegisterPassword(e.target.value)} type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" placeholder='Write your password here'/>
               </div>    
 
-              <button type="submit" className="w-full text-black bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+              <button onClick={() => onRegisterSubmit()} type="submit" className="w-full text-black bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign in here </a>
+                  Already have an account? <a href="#" onClick={handleLoginLinkClick} className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign in here </a>
               </p>
             </form>    
           </div>
--- src/elements/RegisterPengacara/RegisterFormPengacara.jsx
+++ src/elements/RegisterPengacara/RegisterFormPengacara.jsx
@@ -1,6 +1,6 @@
-// import useNavigate from 'react-router-dom';
-// import { useState } from 'react';
-// import axios from 'axios';
+import useNavigate from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import React from 'react';
 
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -8,43 +8,62 @@ import React from 'react';
 
 const RegisterFormPengacara = () => {
 
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const navigate = useNavigate();
+  const [token, setToken] = useState(null)
+  const [emailPengacara, setEmailPengacara] = useState('');
+  const [passwordPengacara, setPasswordPengacara] = useState('');
+  const [namaPengacara, setNamaPengacara] = useState('');
+  const [addressPengacara, setAddressPengacara] = useState('');
+  const [pendidikanPengacara, setPendidikanPengacara] = useState(''); // eslint-disable-line no-unused-vars
+  const [ktpPengacara, setKtpPengacara] = useState(''); // eslint-disable-line no-unused-vars
+  const navigate = useNavigate();
 
-  // const handleRegisterLinkClick = (e) => {
-  //   e.preventDefault();
-  //   const data = {email, password};
-  //   console.log(data);
-  //   navigate('/');
-  // }
+  const handleLoginLinkClick = (e) => {
+    e.preventDefault();
+    const data = {emailPengacara, passwordPengacara, namaPengacara, addressPengacara, pendidikanPengacara, ktpPengacara};
+    console.log(data);
+    navigate('/login/pengacara');
+  }
 
-  // // eslint-disable-next-line no-undef
-  // const URL = process.env.REACT_APP_BE_ENDPOINT
-  // console.log('URL', URL)
+  // eslint-disable-next-line no-undef
+  const URL = process.env.REACT_APP_BE_ENDPOINT
+  console.log('URL', URL)
 
-  // const onLoginSubmit = () => {
-  //   console.log(`email: ${email}, password: ${password}`)
-  //     axios.post(`${URL}/v1/auth/login`, {
-  //       email,
-  //       password
-  //     })
-  //       .then(function (response) {
-  //         console.log('login success', response);
-  //           if (response && response.data && response.data.tokens && response.data.tokens.access) {
-  //             localStorage.setItem('token', JSON.stringify(response.data.tokens.access.token))
-  //             localStorage.setItem('user', JSON.stringify(response.data.user))
-  //             navigate('/homepage')
-  //           }
-  //       })
-  //         .catch(function (error) {
-  //           console.error('Login error', error.message);
-  //            window.alert('error', 'Login Failed', `<b>[CODE] ${error.code}</b><br>Please check your username and password`);
-  //           }
-  //       );
-  // }
+  const onRegisterSubmit = () => {
+    console.log(`name: ${namaPengacara}, email: ${emailPengacara}, password: ${passwordPengacara}, address:${addressPengacara}, ktp: ${ktpPengacara}`)
+    axios.post(`${URL}/v1/auth/register/pengacara`, {
+      name: namaPengacara,
+      email: emailPengacara,
+      ktp: ktpPengacara,
+      address: addressPengacara,
+      pendidikan: pendidikanPengacara,
+      password: passwordPengacara
+    })
+      .then(function (response) {
+        console.log('register success', response);
+        if (response && response.data && response.data.tokens && response.data.tokens.access) {
+          localStorage.setItem('token', JSON.stringify(response.data.tokens.access.token))
+          localStorage.setItem('user', JSON.stringify(response.data.user))
+          navigate ('/login/pengacara');
+        }
+      })
+      .catch(function (error) {
+        if (error.response && error.response.status === 400 && error.response.data.message) {
+            window.alert('error', 'Registration Failed', `<b>[CODE] ${error.response.data.message}</b><br>Please check your username and password`);
+        } else {
+            window.alert('error', 'Registration Failed', `<b>[CODE] </b><br>An error occured during registration. Please try again later`);
+        }
+        console.error('Registration error', error.message);
+     }
+     );
+   }
 
+  
+  useEffect(() => {
+    const tokenFromLS = localStorage.getItem('token')
+    setToken(tokenFromLS)
+  }, [])
 
+  console.log('token', token)
 
 
   return (
@@ -58,27 +77,27 @@ const RegisterFormPengacara = () => {
             <form className="space-y-4 md:space-y-6" action="">
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Full Name</label>
-                <input type="namapengacara" name="namapengacara" id="namapengacara" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your full name here' required=""/>
+                <input onChange={(e) => setNamaPengacara(e.target.value)} type="namapengacara" name="namapengacara" id="namapengacara" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your full name here' required=""/>
               </div>
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
-                <input type="emailpengacara" name="emailpengacara" id="emailpengacara" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your email here' required=""/>
+                <input onChange={(e) => setEmailPengacara(e.target.value)} type="emailpengacara" name="emailpengacara" id="emailpengacara" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your email here' required=""/>
               </div>
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your NIK (KTP)</label>
-                <input type="no_ktpa" name="no_ktpa" id="no_ktpa" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your NIK here' required=""/>
+                <input onChange={(e) => setKtpPengacara(e.target.value)}type="no_ktpa" name="no_ktpa" id="no_ktpa" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your NIK here' required=""/>
               </div>
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Address</label>
-                <input type="alamat" name="alamat" id="alamat" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your Address here' required=""/>
+                <input onChange={(e) => setAddressPengacara(e.target.value)}type="alamat" name="alamat" id="alamat" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Write your Address here' required=""/>
               </div>
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your University Degree</label>
-                <input type="pendidikan" name="pendidikan" id="pendidikan" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Name of the University You Attended' required=""/>
+                <input onChange={(e) => setPendidikanPengacara(e.target.value)}type="pendidikan" name="pendidikan" id="pendidikan" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Name of the University You Attended' required=""/>
               </div>
 
               <div>
@@ -139,12 +158,12 @@ const RegisterFormPengacara = () => {
 
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" placeholder='Write your password here'/>
+                <input onChange={(e) => setPasswordPengacara(e.target.value)} type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" placeholder='Write your password here'/>
               </div>    
 
               <button type="submit" className="w-full text-black bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign in here </a>
+                  Already have an account? <a href="#" onClick={handleLoginLinkClick} className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign in here </a>
               </p>
             </form>    
           </div>
