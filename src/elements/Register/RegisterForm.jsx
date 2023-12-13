de62cf7bc90df9b1c75cc7f2a15c0ee61579e75a
kresna <kresnafti2013@gmail.com>
2023-12-13 13:30:30 +0700

    fix(lawyer)(user): refactor login, register for user and fix login for lawyer

--- src/elements/Register/RegisterForm.jsx
+++ src/elements/Register/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import useNavigate from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import React from 'react';
