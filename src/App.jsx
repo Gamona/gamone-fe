cedc67cfa2efe48f2b690407d98f57815d3ffa34
kresna <kresnafti2013@gmail.com>
2023-12-15 19:47:20 +0700

    feat(route):add public route and private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,34 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { RouterProvider, createBrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import RegisterPengacara from './pages/RegisterPengacara'
 import LoginPengacara from './pages/LoginPengacara'
 import Chats from './pages/Chats'
 import ChatsLawyer from './pages/ChatLawyer'
+import { PrivateRoutes, PublicRoutes } from './route'
 
 const App = () => {
 
-  const router = createBrowserRouter([
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/register',
-      element: <Register />
-    },
-    {
-      path: '/register/pengacara',
-      element: <RegisterPengacara />
-    },
-    {
-      path: '/login/pengacara',
-      element: <LoginPengacara />
-    },
-    {
-      path: '/chats',
-      element: <Chats />
-    },
-    {
-      path: '/chatsLawyer',
-      element: <ChatsLawyer />
-    },    
-  ])
+  const [status, setStatus] = useState(false)
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('token'));
+    if(items) {
+      setStatus(true) 
+    }
+  }, [])
+
   return (
-    <RouterProvider router={router} />
+        <Routes>
+        {
+          status
+               ? <Route path="/*" element={<PrivateRoutes />} />
+               : <Route path="/*" element={<PublicRoutes />} />
+        }
+
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
   )
 }
 
